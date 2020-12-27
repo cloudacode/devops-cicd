f96a9e90c3200f0c2fcd07e695beb698f97db5d2
kcfigaro <kcfigaro@gmail.com>
2020-12-27 08:41:23 +0100

    updated week1 value

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, DevOps!'
+    return 'Hello, DevOps, Week1!'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8000)
